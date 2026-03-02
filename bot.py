e6aa8aa2fd17e1745ccb44da3c4858b6263d56f4
Mrkingbotapi927 <khanaam200@gmail.com>
2026-03-02 19:33:13 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ SUPER_OWNER_ID = 8382316368
 
 IVAS_EMAIL = "iamalisindhi1122@gmail.com"
 IVAS_PASSWORD = "Shoaibali@123D..king"
-IVAS_URL = "https://ivas.tempnum.qzz.io/portal"
+IVAS_URL = "https://ivas.tempnum.qzz.io/portal/live/my_sms"
 
 bot = telebot.TeleBot(API_TOKEN)
 
